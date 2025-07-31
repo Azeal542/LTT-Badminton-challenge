0b3dcffbcb75ce9a16c78587b03c29ad3150c786
Caleb Pierce <calebrpierce@gmail.com>
2025-07-31 13:44:30 -0700

    Implemented ELO system

--- Player management.py
+++ Player management.py
@@ -5,11 +5,37 @@ datasheet = "Player sheet.csv"
 data = pd.read_csv(datasheet)
 #print(data)
 def create_user():
-    name = input("Enter player name:", )
+    name = input("Enter player name:", ).upper()
     last_id = data['UserID'].iloc[-1]
     next_id = int(last_id) + 1
 
-    new = pd.DataFrame([[next_id, name, 1500]], columns=['UserID', 'NAME', 'ELO'])
+    new = pd.DataFrame([[next_id, name, 1500]], columns=['UserID', 'Name', 'ELO'])
     new.to_csv(datasheet, mode='a', index=False, header=False)
 
-create_user()
+def elo_calc():
+    player1 = input("Enter winner's name: ").upper()
+    player2 = input("Enter loser's name: ").upper()
+
+    winner = data.loc[data['Name'] == player1]
+    loser =  data.loc[data['Name'] == player2]
+    
+    A = int(winner['ELO'].iloc[-1])
+    B = int(loser['ELO'].iloc[-1])
+    diff = B - A
+    div = diff / 400
+    exp = 1 + (10**div)
+    dev = 1/exp
+    eval = round(20 * (1 - dev))
+    As = A + eval
+    Bs = B - eval
+    print(As)
+    print(Bs)
+    windex = data.loc[data['Name'] == player1].index[0]
+    losdex = data.loc[data['Name'] == player2].index[0]
+
+    data.loc[windex, 'ELO'] = As
+    data.loc[losdex, 'ELO'] = Bs
+    data.to_csv(datasheet, index=False)
+
+elo_calc()
+#create_user()
